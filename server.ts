dc639c4da276338954b6ace13573abfbde2d77b6
Imtiaz Zahir <imtiazzahir43@gmail.com>
2024-05-09 10:27:08 +0600

    first commit

---
+++ server.ts
@@ -0,0 +1,67 @@
+import sharp from "sharp";
+import * as axios from "axios";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+import uploadToS3 from "./uploadTos3";
+
+async function downloadImage(url: string): Promise<Buffer> {
+  const response = await axios.default({
+    method: "GET",
+    url: url,
+    responseType: "arraybuffer",
+  });
+
+  return Buffer.from(response.data, "binary");
+}
+
+async function convertToWebP(
+  imageBuffer: Buffer,
+  width?: number
+): Promise<Buffer> {
+  return sharp(imageBuffer)
+    .resize({
+      width: width,
+      withoutEnlargement: true,
+      fit: sharp.fit.inside,
+    })
+    .toFormat("webp")
+    .toBuffer();
+}
+
+async function main() {
+  const images = await prisma.photos.findMany({
+    take: 50,
+    where:{displayImage:{not:{startsWith:"https://ai-photos-storage.s3-ap-southeast-2.amazonaws.com"}}},
+    select: { id: true, prompt: true, displayImage: true },
+  });
+
+  for (const image of images) {
+
+    try {
+        console.log("Downloading image...");
+        const imageBuffer = await downloadImage(image.displayImage);
+        console.log("Downloading complete");
+        const webP400 = await convertToWebP(imageBuffer, 400);
+        const webPFull = await convertToWebP(imageBuffer);
+        console.log(
+          "Conversion complete. Now you can save or use the WebP image buffer."
+        );
+        const urls = await uploadToS3(image.prompt, webP400, webPFull);
+        await prisma.photos.update({
+          where: { id: image.id },
+          data: {
+            thumbnailImage: urls.w400,
+            displayImage: urls.wFull,
+          },
+        });
+        console.log("Uploaded to S3");
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+  }
+  const count = await prisma.photos.count({where:{displayImage:{not:{startsWith:"https://ai-photos-storage.s3-ap-southeast-2.amazonaws.com"}}}});
+  console.log(`There are ${count} images left to convert.`);
+  main();
+}
+
+main();
---
+++ uploadTos3.ts
@@ -0,0 +1,50 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import 'dotenv/config'
+
+export const s3Client = new S3Client({
+  region: "ap-southeast-2",
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY as string,
+  },
+});
+
+export default async function uploadToS3(
+  name: string,
+  w400: Buffer,
+  wFull: Buffer
+) {
+  const cleanText = name.replace(/[^\w\s]/g, "");
+  name = cleanText.split(/\s+/).slice(0, 10).join("-");
+
+  const w400Name = name + "-400-" + new Date().getTime() + ".webp";
+  const wFullName = name + "-" + new Date().getTime() + ".webp";
+  const up400 = new PutObjectCommand({
+    Bucket: "ai-photos-storage",
+    Key: w400Name,
+    Body: w400,
+    ContentType: "image/webp",
+    ContentLength: w400.length,
+  });
+
+  const upFull = new PutObjectCommand({
+    Bucket: "ai-photos-storage",
+    Key: wFullName,
+    Body: wFull,
+    ContentType: "image/webp",
+    ContentLength: wFull.length,
+  });
+
+  try {
+    await s3Client.send(up400);
+    await s3Client.send(upFull);
+
+    return {
+      w400: `https://ai-photos-storage.s3-ap-southeast-2.amazonaws.com/${w400Name}`,
+      wFull: `https://ai-photos-storage.s3-ap-southeast-2.amazonaws.com/${wFullName}`,
+    };
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error uploading to S3");
+  }
+}
